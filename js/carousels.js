ac41b80aa56e4be27bfc9f66dbb45f8871465fe4
createhb21 <alsghk9701@gmail.com>
2022-04-18 17:16:36 +0900

    implement user-gallery components

--- js/carousels.js
+++ js/carousels.js
@@ -15,3 +15,24 @@ const productCarousel = tns({
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
 })
+
+const userGalleryMobile = tns({
+  ...commonOptions,
+  container: '.user-gallery.is-mobile .slider-list',
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
+  loop: false,
+})
+
+const userGalleryDesktop = tns({
+  ...commonOptions,
+  container: '.user-gallery.is-desktop .slider-list',
+  gutter: 6,
+  edgePadding: 75,
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  loop: false,
+})
